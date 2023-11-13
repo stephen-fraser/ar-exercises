6e98af219ac99e6bdef0b2141d437db1ccf7340a
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-13 15:53:46 -0800

    completed exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,38 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+Store.create(
+  name: "Surrey",
+  annual_revenue: 224000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Store.create(
+  name: "Whistler",
+  annual_revenue: 1900000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+Store.create(
+  name: "Yaletown",
+  annual_revenue: 430000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+puts Store.count
+
+@mens_stores = Store.where('mens_apparel: true')
+
+for store in @mens_stores do
+  puts "Store Name: #{store.name} | Annual Rev: #{store.annual_revenue}"
+end
+
+@womens_stores = Store.where('womens_apparel = true').where('annual_revenue < ?', 1000000)
+
+for store in @womens_stores do
+  puts "Store Name: #{store.name} | Annual Rev: #{store.annual_revenue}"
+end
