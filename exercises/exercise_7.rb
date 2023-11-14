4def45f8633f78d295047430b7657aa5f4dee249
stephen-fraser <steve.james.fraser@gmail.com>
2023-11-13 18:13:59 -0800

    exercise 7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,9 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+
+puts "Store name please:  "
+store_name = gets.chomp
+
+new_store = Store.create(name: store_name)
+p new_store.errors.full_messages
